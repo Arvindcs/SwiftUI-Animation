5acac22163ff21f5ac518bc206d372e820cbfa04
Arvind <arvindp@alohatechnologydev.com>
2020-12-19 09:52:03 +0530

    Update Rotaion Loading

--- CircleRotation/CircleRotation/ContentView.swift
+++ CircleRotation/CircleRotation/ContentView.swift
@@ -8,63 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isShowAnimation = false
-    @State private var rotationDegree = 0.0
-    
-    @State private var pickerSelectedItem = 0
-    @State private var dataPoints: [[CGFloat]] = [
-        [100, 120, 150],
-        [150, 100, 120],
-        [120, 150, 100],
-        [150, 100, 120],
-        [100, 120, 150],
-    ]
+    @State private var rotationDegree = 0
+    @State private var isAnimation = false
+    let mainCirle: CGFloat = 170
+    let rotationLine: CGFloat = 190
+    let capshuleWidth: CGFloat = 12
     
     var body: some View {
         ZStack {
-            Circle()
-                .fill(Color.getCircleColor())
-                .frame(width: mainCircle, height: mainCircle)
-           
-            HStack(alignment: .center, spacing: 8) {
-                Capsule()
-                    .fill(Color.white)
-                    .frame(width: capsuleWidth, height: dataPoints[pickerSelectedItem][0])
-                Capsule()
-                    .fill(Color.white)
-                    .frame(width: capsuleWidth, height: dataPoints[pickerSelectedItem][1])
-                Capsule()
-                    .fill(Color.white)
-                    .frame(width: capsuleWidth, height: dataPoints[pickerSelectedItem][2])
-                Capsule()
-                    .fill(Color.white)
-                    .frame(width: capsuleWidth, height: dataPoints[pickerSelectedItem][1])
-                Capsule()
-                    .fill(Color.white)
-                    .frame(width: capsuleWidth, height: dataPoints[pickerSelectedItem][0])
-            }
-            .frame(height: 100, alignment: .center)
+            Circle().fill(Color.getBallColor())
+                .frame(width: mainCirle, height: mainCirle)
+            
+            HStack(alignment: .center, spacing: 8){
+                
+                Capsule().fill(Color.white)
+                    .frame(width: capshuleWidth, height: isAnimation ? 100 : 0)
+                Capsule().fill(Color.white)
+                    .frame(width: capshuleWidth, height: isAnimation ? 120 : 0)
+                Capsule().fill(Color.white)
+                    .frame(width: capshuleWidth, height: isAnimation ? 150 : 0)
+                Capsule().fill(Color.white)
+                    .frame(width: capshuleWidth, height: isAnimation ? 130 : 0)
+                Capsule().fill(Color.white)
+                    .frame(width: capshuleWidth, height: isAnimation ? 100 : 0)
+                        
+                
+            }.frame(height: 100, alignment: .center)
             .animation(Animation.linear(duration: 0.5).repeatForever())
             
             Circle()
-                .trim(from: 0 , to: 1 )
-                .stroke(Color.getCircleColor(), style:
-                            StrokeStyle(lineWidth: 5, lineCap: .round,
-                            dash: [30, 30], dashPhase: 40))
-                
+                .trim(from: 0.0, to: 1.0)
+                .stroke(Color.getBallColor(), style: StrokeStyle(lineWidth: 5, lineCap: .round, dash: [30, 30], dashPhase: 40))
+                .rotationEffect(.degrees(Double(rotationDegree)))
                 .frame(width: rotationLine, height: rotationLine)
-                .rotationEffect(.degrees(rotationDegree))
-                .animation(Animation.linear(duration: 3)
-                .repeatForever(autoreverses: false))
+                .animation(Animation.linear(duration: 3).repeatForever(autoreverses: false))
         }.onAppear() {
             rotationDegree = -360
-            isShowAnimation.toggle()
+            isAnimation.toggle()
         }
     }
-    let mainCircle: CGFloat = 170
-    let rotationLine: CGFloat = 190
-    let capsuleWidth: CGFloat = 12
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
